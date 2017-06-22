e313505caaf29dff87bef6d1ec16bb543d549d2e
horato <horato@seznam.cz>
2017-06-22 22:56:19 +0200

    OutputView - scrollbar

--- RoxorBot/Modules/Output/Views/OutputView.xaml.cs
+++ RoxorBot/Modules/Output/Views/OutputView.xaml.cs
@@ -7,9 +7,34 @@ namespace RoxorBot.Modules.Output.Views
     /// </summary>
     public partial class OutputView : UserControl
     {
+        private bool _autoScroll = true;
+
         public OutputView()
         {
             InitializeComponent();
+        }
+
+        private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var tb = sender as TextBox;
+            tb?.ScrollToEnd();
+        }
+
+        private void ScrollViewer_OnScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var viewer = sender as ScrollViewer;
+            if (viewer == null)
+                return;
+
+            if (e.ExtentHeightChange == 0)
+            {
+                _autoScroll = viewer.VerticalOffset == viewer.ScrollableHeight;
+            }
+
+            if (_autoScroll && e.ExtentHeightChange != 0)
+            {
+                viewer.ScrollToVerticalOffset(viewer.ExtentHeight);
+            }
         }
     }
 }
